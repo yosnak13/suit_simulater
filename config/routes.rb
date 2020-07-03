b04f1216cdc11507d10d14d39b9c6c152470bad4
“yosnak13” <yosnak13@gmail.com>
2020-07-03 23:13:48 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get '/signup', to:'users#new'
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root'application#hello'
 end
