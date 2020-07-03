4c4d20994ac1e71a8f6fc275ac8284e1497dbd3c
“yosnak13” <yosnak13@gmail.com>
2020-07-03 12:46:12 +0900

    create login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  validates :gender, presence: true
-  validates :age, presence: true
+  # validates :gender, presence: true
+  # validates :age, presence: true
 end
