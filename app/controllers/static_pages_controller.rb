65a1308e7eb9e76d097506e63c09720a7591ac87
“yosnak13” <yosnak13@gmail.com>
2020-07-08 00:36:06 +0900

    create 3 pages for items

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def suit
+  end
+
+  def shirt
+  end
+
+  def tie
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get '/signup', to:'users#new'
+  get 'sessions/new'
   resources :users
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  #アイテム検索用ルーティング
+  get '/suit', to: 'static_pages#suit'
+  get '/tie', to: 'static_pages#tie'
+  get '/shirt', to: 'static_pages#shirt'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root'application#hello'
 end
