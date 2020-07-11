31749312ddd7b3c707536b53428ef2e086c4f7a4
“yosnak13” <yosnak13@gmail.com>
2020-07-12 00:08:24 +0900

    scss分離の準備

--- app/controllers/hopes_controller.rb
+++ app/controllers/hopes_controller.rb
@@ -1,4 +1,8 @@
 class HopesController < ApplicationController
+  def index
+    @partial_css_disabled = true
+  end
+
   def suit
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def partial_stylesheet_link_tag(controller_name, action_name, disabled = true)
+    if disabled == false || disabled.nil?
+      if File.exist?("#{Rails.root.to_s}/app/assets/stylesheets/#{controller_name}/#{action_name}.css.scss")
+        return stylesheet_link_tag "#{controller_name}/#{action_name}"
+      end
+    end
+  end
 end
