296f77740fd3fd723d9590101a8a670f86b87f3c
“yosnak13” <yosnak13@gmail.com>
2020-07-18 14:06:48 +0900

    これからroutesとか直すとこ

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,12 @@ Rails.application.routes.draw do
   get '/signup', to:'users#new'
   get 'sessions/new'
 
-  # resources :users do
-  #   member do
-  #     get '/password_reset', to: 'password_resets#new'
-  #     post '/password_reset', to: 'password_resets#create'
-  #   end
-  # end
-
-  resource :users
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :users do
+    member do
+      get '/password_reset', to: 'password_resets#new'
+      post '/password_reset', to: 'password_resets#create'
+    end
+  end
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
@@ -24,6 +21,4 @@ Rails.application.routes.draw do
   get '/tie', to: 'hopes#tie'
 
   get '/select_suit', to: 'hopes#select_suit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root'application#hello'
 end
