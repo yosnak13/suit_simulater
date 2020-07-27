7eb4c686ebd638517435d497630eee9ea7b94956
“yosnak13” <yosnak13@gmail.com>
2020-07-27 14:27:09 +0900

    fontfamilyの確定

--- db/schema.rb
+++ db/schema.rb
@@ -21,9 +21,6 @@ ActiveRecord::Schema.define(version: 20200717034956) do
     t.string "gender"
     t.string "age"
     t.string "remember_digest"
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
