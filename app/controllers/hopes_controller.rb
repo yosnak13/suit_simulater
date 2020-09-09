5213d32742812a5eb47abd8e006c8d57b8fea8d2
“yosnak13” <yosnak13@gmail.com>
2020-09-09 09:02:36 +0900

    results_controller作成中、大まかなメソッドが見えてきた

--- app/controllers/hopes_controller.rb
+++ app/controllers/hopes_controller.rb
@@ -19,14 +19,20 @@ class HopesController < ApplicationController
   end
 
   def create
-    @hope_suits = HopeSuit.new
+    @hope_suits = current_user.hope_suits.new(suits_params) #user_id
     #@hope_suitsにはidが必要？引数はidで合ってる？
     if @hope_suits.save
+      flash[:success] = "成功しました！"
       redirect_to result_suit_user_path
     else
       flash.now[:notice] = "エラー：色を選んで下さい"
       render 'select_suit'
     end
   end
+end
+
+  private
+def suits_params
+  params.require(:hope_suit).permit(:suit_color)
 end
 #@user ---<- @hope_suits
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,11 +1,27 @@
 class ResultsController < ApplicationController
-  # MAX_DISPLAY_RELATED_PRODUCTS = 4
-  def result_suits
+  # MAX_DISPLAY_RELATED_PRODUCTS = 3
+  def result_suit
+    @hope_suits = current_user.hope_suits.last
     # if params[:hope_suits] == "チェック" || "ウィンドウペン"
     #   @shrit = Shirt.where(color: "白")
     # else
     #   @shirt = Shirt.where(color: "白").shuffle.take(MAX_DISPLAY_RELATED_PRODUCTS)
     # end
+    case @hope_suits.suit_color
+    when "ネイビー"
+      @reccomend = "あなたの選んだ色はネイビーなので、あなたに合うシャツは＿＿です"
+      puts <%= image_tag "navy1.jpg", alt:"ネイビー", class:"navy section size" %>
+    when "グレー"
+      @reccomend = "あなたの選んだ色はグレーなので、あなたに合うシャツは＿＿です"
+    when "ベージュ"
+      @reccomend = "あなたの選んだ色はベージュなので、あなたに合うシャツは＿＿です"
+    when "ブラック"
+      @reccomend = "あなたの選んだ色はブラックなので、あなたに合うシャツは＿＿です"
+    when "チェック"
+      @reccomend = "あなたの選んだ色はチェックなので、あなたに合うシャツは＿＿です"
+    when "ウィンドウペン"
+      @reccomend = "あなたの選んだ色はウィンドウペンなので、あなたに合うシャツは＿＿です"
+    end
   end
 
   def result_shirt
