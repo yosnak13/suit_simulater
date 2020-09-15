d9a4c3cba6a26fbd38379699afeff9612481239d
“yosnak13” <yosnak13@gmail.com>
2020-09-15 19:57:39 +0900

    レッスン前

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -12,4 +12,20 @@ module ResultsHelper
     end
   end
 
+  def recomemded_shirt
+    if @hope_suits.suit_color == "チェック" || @hope_suits.suit_color == "ウィンドウペン"
+      # URI("/assets/beige1.jpg")
+      URI("https://www.llbean.co.jp/on/demandware.static/-/Sites-llbean-master-catalog/ja_JP/dw823ded91/images/Product/256513_00047_S.JPG")
+    else
+      URI("https://www.llbean.co.jp/dw/image/v2/BCSL_PRD/on/demandware.static/-/Sites-llbean-master-catalog/ja_JP/dw2cc95042/images/Product/209411_00047_S.JPG?sw=539")
+    end
+  end
+
+  def recomemded_tie
+    if @hope_suits.suit_color == "チェック" || @hope_suits.suit_color == "ウィンドウペン"
+      URI("")
+    else
+      URI("")
+    end
+  end
 end
