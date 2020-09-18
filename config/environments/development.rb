e3e17586946cf60a6dda9b1bf0a8e2e7b86846c0
“yosnak13” <yosnak13@gmail.com>
2020-09-18 18:20:00 +0900

    production.rbのhostのurl変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'https://polar-mesa-36797.herokuapp.com/'
+  host = 'https://polar-mesa-36797.herokuapp.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
