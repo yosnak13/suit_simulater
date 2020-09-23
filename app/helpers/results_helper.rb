929682cf0ae20f8678135ed72b74a3a3bff1b398
“yosnak13” <yosnak13@gmail.com>
2020-09-23 21:40:18 +0900

    merge conflict解決と、helperの動的表現を追加

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -12,4 +12,27 @@ module ResultsHelper
     end
   end
 
+  def xxx(obj)
+    if obj == "ネイビー"
+      "ネイビースーツが選ばれました"
+    elsif obj == "グレー"
+      "グレースーツが選ばれました"
+    elsif obj == "ブラック"
+      "ブラックスーツが選ばれました"
+    elsif obj == "ベージュ"
+      "ベージュスーツが選ばれました"
+    elsif obj == "チェック"
+      "チェックスーツが選ばれました"
+    elsif obj == "ウィンドウペン"
+      "ウィンドウペンスーツが選ばれました"
+    end
+  end
+  # def recomemded_shirt
+  #   if @hope_suits.suit_color == "チェック" || @hope_suits.suit_color == "ウィンドウペン"
+  #     URI("https://www.llbean.co.jp/on/demandware.static/-/Sites-llbean-master-catalog/ja_JP/dw823ded91/images/Product/256513_00047_S.JPG")
+  #   else
+  #     URI("https://www.llbean.co.jp/dw/image/v2/BCSL_PRD/on/demandware.static/-/Sites-llbean-master-catalog/ja_JP/dw2cc95042/images/Product/209411_00047_S.JPG?sw=539")
+  #   end
+  # end
+
 end
