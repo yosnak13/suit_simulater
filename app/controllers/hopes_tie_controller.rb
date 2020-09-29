950d6d0606aabb109ccbe3b42c03a63333483169
“yosnak13” <yosnak13@gmail.com>
2020-09-29 14:33:09 +0900

    ネクタイ選択ページの雛形まで到達

--- app/controllers/hopes_tie_controller.rb
+++ app/controllers/hopes_tie_controller.rb
@@ -1,7 +1,21 @@
 class HopesTieController < ApplicationController
   def select_tie
+    @hope_ties = HopeTie.new
   end
 
   def create
+    @hope_tie = current_user.hope_tie.new(ties_params)
+    unless ties_params[:tie_pattern].present?
+      flash.now[:notice] = "エラー：色を選んで下さい"
+      render 'select_tie' and return
+    end
+    @hope_tie.save
+    flash[:success] = "こちらです！"
+    redirect_to result_tie_user_path
+  end
+
+  private
+  def ties_params
+    params.require(:hope_shirt).permit(:tie_pattern)
   end
 end
