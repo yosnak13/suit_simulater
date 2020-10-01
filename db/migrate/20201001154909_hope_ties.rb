cded177dbe93e0f68ca5689cfcf27dbf644e56c2
“yosnak13” <yosnak13@gmail.com>
2020-10-02 00:50:56 +0900

    Hope_tiesテーブルをschemaから削除完了

---
+++ db/migrate/20201001154909_hope_ties.rb
@@ -0,0 +1,5 @@
+class HopeTies < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :hope_ties
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201001152402) do
+ActiveRecord::Schema.define(version: 20201001154909) do
 
   create_table "hope_shirts", force: :cascade do |t|
     t.string "shirt_pattern"
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 20201001152402) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_hope_tie_patterns_on_user_id"
-  end
-
-  create_table "hope_ties", force: :cascade do |t|
-    t.string "tie_pattern"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_hope_ties_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
