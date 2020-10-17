d0e71f048f4ba2e2e3be37ce787ad0c18432af56
“yosnak13” <yosnak13@gmail.com>
2020-10-17 13:20:04 +0900

    viewに記載していたjsを全てscript.jsに移動

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,5 +9,47 @@ $(document).on('turbolinks:load', function () {
       }
     });
   });
-  
+  //hopes_view
+  $(function () {
+    //クリックした要素のidをsubmitのvalueにする
+    $(".sub-box").on("click", function () {
+      var result = $(this).attr("id");
+      document.querySelector("#submit").value = result;
+    });
+
+    //image_tagを隠し、色をクリックしたら出現させる
+    $(".section").hide();
+    $(".secList").on("click", function () {
+      // クリックした要素の ID と違うクラス名のセクションを非表示
+      $(".section").not($("." + $(this).attr("id"))).hide();
+      // クリックした要素の ID と同じクラスのセクションを表示
+      $("." + $(this).attr("id")).fadeIn(500);
+    });
+
+    //ホバーアクション
+    $(".secList").hover(function () {
+      $(this).css("height", "70px");
+      $(this).css("width", "70px");
+      $(this).css("margin-left", "10px");
+      $(this).css("margin-right", "10px");
+    },
+      function () {
+        $(this).css("height", "90px");
+        $(this).css("width", "90px");
+        $(this).css("margin-left", "0px");
+        $(this).css("margin-right", "0px");
+      });
+  });
+
+  //results_view
+  $(window).scroll(function () {
+    $('.fadein').each(function () {
+      var elemPos = $(this).offset().top,
+        scroll = $(window).scrollTop(),
+        windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 200) {
+        $(this).addClass('scrollin');
+      }
+    });
+  });
 });
