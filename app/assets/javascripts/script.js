c45fb41bef6736c0e839db83388aac9c6e7fbf07
“yosnak13” <yosnak13@gmail.com>
2020-10-18 15:32:31 +0900

    result画面fadein速度、font変更、home背景画像レスポンシブ、home画面変更中

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -47,7 +47,7 @@ $(document).on('turbolinks:load', function () {
       var elemPos = $(this).offset().top,
         scroll = $(window).scrollTop(),
         windowHeight = $(window).height();
-      if (scroll > elemPos - windowHeight + 200) {
+      if (scroll > elemPos - windowHeight + 50) {
         $(this).addClass('scrollin');
       }
     });
