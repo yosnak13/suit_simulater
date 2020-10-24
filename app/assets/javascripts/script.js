a17108704fdcc8d6eeeef9d2c7c7b6c4f2b64c48
“yosnak13” <yosnak13@gmail.com>
2020-10-24 16:40:49 +0900

    home pageにスライドショー昨日を実装

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,21 @@
 $(document).on('turbolinks:load', function () {
+  $(function () {
+    /* SP menu */
+    function toggleNav() {
+      var body = document.body;
+      var hamburger = document.getElementById('nav_btn');
+      var blackBg = document.getElementById('nav_bg');
+      hamburger.addEventListener('click', function () {
+        body.classList.toggle('nav_open'); //メニュークリックでnav-openというクラスがbodyに付与
+      });
+      blackBg.addEventListener('click', function () {
+        body.classList.remove('nav_open'); //もう一度クリックで解除
+      });
+    }
+    toggleNav();
+  });
+  
+
   $(window).scroll(function () {
     $('.fadein').each(function () {
       var elemPos = $(this).offset().top,
